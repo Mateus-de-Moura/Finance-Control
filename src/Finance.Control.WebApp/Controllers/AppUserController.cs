761cdb5614d775c7a54ffff08c5c285911c8e88e
Mateus-de-Moura <mateus.demoura@hotmail.com>
2024-11-01 16:54:36 -0300

    Feat: criando  rotas de cadastro de usuários

---
+++ src/Finance.Control.WebApp/Controllers/AppUserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Finance.Control.webApp.Controllers
+{
+    public class AppUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
